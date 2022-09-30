83a596a67184c424bc1e40387732b413532df7b5
Zennay <112865109+Zennay@users.noreply.github.com>
2022-09-30 09:54:52 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-var deButton = document.querySelector("nav button");
+var deButton = document.querySelector("header a:first-child");
+var deCloseButton = document.querySelector("header a:nth-of-type(2)");
 
-deButton.addEventListener("click", toggleMenu);
+deButton.addEventListener("click", toggleMenu,);
 
 function toggleMenu() {
-    var deNav = document.querySelector("nav");
+    var deButton = document.querySelector("header a:first-child");
     deNav.classList.toggle("toonMenu");
+}
+
+function closeMenu() {
+    var deCloseButton = document.querySelector("header button:nth-of-type(2)");
+    deCloseNav.classList.toggle("closeMenu");
 }
